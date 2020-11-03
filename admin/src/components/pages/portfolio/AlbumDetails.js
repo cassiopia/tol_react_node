df952e51b469a8db349b6b23e90609ee7c3f8a31
cassiopia <cassiopia@yandex.ru>
2020-11-03 17:52:07 +0300

    - Создала форму для редактирования альбома включая тэги и редактор текста

--- admin/src/components/pages/portfolio/AlbumDetails.js
+++ admin/src/components/pages/portfolio/AlbumDetails.js
@@ -1,9 +1,25 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React from 'react';
 import axios from 'axios';
 import Breadcrumbs from "../../breadcrumbs/Breadcrumbs";
 import {useLocation} from 'react-router-dom'
-import {Button} from 'react-bootstrap';
-import './css/style.css'
+import {useForm} from "react-hook-form";
+import {Form, Input, Button} from 'antd';
+import {useQuill} from 'react-quilljs';
+import EditableTagGroup from './EditableTagGroup';
+import 'quill/dist/quill.snow.css';
+import './css/style.css';
+import 'antd/dist/antd.css'
+
+
+// todo Наверное можно сделать это как-то изящнее, но пока так
+const buttonStyle = {
+    "backgroundColor": "#75b209",
+    "border": "#75b209"
+};
+
+const divButtonsStyle = {
+    "textAlign": "center"
+};
 
 // todo Разобраться как нода должна общаться с базой
 // todo редусмотреть что в описание может быть ссылка на отчет. Сделать атк что бы привела в нужный рздел
@@ -11,6 +27,18 @@ import './css/style.css'
 
 
 export default function AlbumDetails() {
+    const [form] = Form.useForm();
+    const {quill, quillRef} = useQuill();
+
+    const onFinish = (values) => {
+        console.log(values);
+        console.log(quill.getText());
+    };
+
+    const onReset = () => {
+        form.resetFields();
+    };
+
     // todo Предусмотреть ссылку для возврата к списку альбомов
     // todo Подумать стоит ли это обозначать в меню
 
@@ -24,8 +52,32 @@ export default function AlbumDetails() {
 
     // todo придумать интерфес для редактирования
     // todo Добавить логику, если в БД пусто, то отображать то что пришло из запроса
-    const albumTitle =  query.get('title');
-    const albumDescription =  query.get('description');
+    const albumTitle = query.get('title');
+
+    //const albumDescription = query.get('description');
+    const albumDescription = 'Пока так, пока не научусь вставлять что-нибудь в базу';
+
+
+    const {register, handleSubmit, setValue} = useForm();
+    const onSubmit = data => console.log(data);
+
+    const handleChange = (e) => {
+        alert(e.target.value);
+        setValue("AntdInput", e.target.value);
+    };
+
+    const editorChange = (e) => {
+        alert(e.target.value);
+        setValue("EditorInput", e.target.value);
+    };
+
+
+    React.useEffect(() => {
+        if (quill) {
+            quill.clipboard.dangerouslyPasteHTML(albumDescription);
+        }
+    }, [quill]);
+
 
     return (
         <>
@@ -34,59 +86,55 @@ export default function AlbumDetails() {
             <div className="row" data-animated="0">
                 <div className="col-md-12">
                     <div id="m-blog-content">
+                        <article className="item">
+                            <div className="mb-thumb">
+                                <img src={albumImg} className="img-responsive" alt=""/>
+                            </div>
 
-                        <div className="row">
-                            <div className="col-md-12">
-                                <article className="item">
-                                    <div className="mb-thumb">
-                                        <img src={albumImg} className="img-responsive" alt=""/>
-                                       
+                            <div className="row">
+                                <div className="tagsDiv col-md-12">
+                                    <EditableTagGroup/>
+                                </div>
+
+                                <Form form={form} name="control-hooks" onFinish={onFinish}>
+
+                                    <div className="col-md-12">
+                                        <Form.Item
+                                            name="title"
+                                            label="Заголовок"
+                                            initialValue={albumTitle}
+                                        >
+                                            <Input/>
+                                        </Form.Item>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <Form.Item
+                                            label="Описание"
+                                        >
+                                            <div>
+                                                <div ref={quillRef}/>
+                                            </div>
+                                        </Form.Item>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <Form.Item style={divButtonsStyle}>
+                                            <Button style={buttonStyle} type="primary" htmlType="submit">
+                                                Submit
+                                            </Button>
+                                            <Button htmlType="button" onClick={onReset}>
+                                                Reset
+                                            </Button>
+                                        </Form.Item>
                                     </div>
 
-                                    <h4>{albumTitle}
-                                        <Button variant="link">
-                                            <div className="pw-inner-portfolio">
-                                                <a href="#"><span><i className="fa fa-edit"></i></span></a>
-                                            </div>
-                                        </Button>
-                                    </h4>
-
-                                    <p>
-                                        <span> {albumDescription} </span>
-                                        <Button variant="link">
-                                            <div className="pw-inner-portfolio">
-                                                <a href="#"><span><i className="fa fa-edit"></i></span></a>
-                                            </div>
-                                        </Button>
-                                    </p>
-
-
-                                    {/*<div className="article-comment-form" data-animated="0">*/}
-                                    {/*    <h5>Leave a Comment</h5>*/}
-                                    {/*    <form>*/}
-                                    {/*        <div className="row">*/}
-                                    {/*            <div className="col-md-6">*/}
-                                    {/*                <span><i className="fa fa-user"></i></span>*/}
-                                    {/*                <input type="text" placeholder="Name">*/}
-                                    {/*                    <span><i className="fa fa-envelope-o"></i></span>*/}
-                                    {/*                    <input type="email" placeholder="e-mail">*/}
-                                    {/*                        <span><i className="fa fa-link"></i></span>*/}
-                                    {/*                        <input type="text" placeholder="website">*/}
-                                    {/*            </div>*/}
-                                    {/*            <div className="col-md-6">*/}
-                                    {/*                <textarea placeholder="Message"></textarea>*/}
-                                    {/*                <button type="submit">Post a Comment</button>*/}
-                                    {/*            </div>*/}
-                                    {/*        </div>*/}
-                                    {/*    </form>*/}
-                                    {/*</div>*/}
-                                </article>
+                                </Form>
                             </div>
-                        </div>
+                        </article>
 
                     </div>
                 </div>
             </div>
+
         </>
-);
+    );
 }
