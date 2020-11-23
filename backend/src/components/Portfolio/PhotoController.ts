0361726c4070abccdf96339daa46a7cace2c3ab6
cassiopia <cassiopia@yandex.ru>
2020-11-23 20:24:31 +0300

    - Добавила типы для request, response, error в PhotoController.ts

--- backend/src/components/Portfolio/PhotoController.ts
+++ backend/src/components/Portfolio/PhotoController.ts
@@ -1,8 +1,8 @@
+import { Request, Response } from 'express';
+import {AxiosError, AxiosResponse} from "axios";
+
 // @ts-ignore
 const axios = require('axios');
-
-// todo Определить типы (вместо any)
-import { Request, Response } from 'express';
 
 const clientId = '6052db0e19156fa';
 
@@ -10,21 +10,21 @@ const accessToken = '3eef1f3a555b4332d4714264d47a16bfb73e5376';
 // https://cassiopiadev.000webhostapp.com/contact.html#access_token=3eef1f3a555b4332d4714264d47a16bfb73e5376&expires_in=315360000&token_type=bearer&refresh_token=d1b75f3199a4a7d40c124bf816a56bd810ab70f8&account_username=triponlife&account_id=126491147
 
 
-exports.getAlbumHashes = function (request: any, response: any) {
+exports.getAlbumHashes = function (request: Request, response: Response) {
 
     axios.get(
         'https://api.imgur.com/3/account/triponlife/albums/ids',
         {'headers': {'Authorization': 'Bearer ' + accessToken}}
     )
-        .then(function (resp: any) {
+        .then(function (resp: AxiosResponse) {
             response.send(resp.data);
         })
-        .catch(function (err: any) {
+        .catch(function (err: AxiosError) {
             response.send(err);
         });
 };
 
-exports.getAlbumInfo = function (request: any, response: any) {
+exports.getAlbumInfo = function (request: Request, response: Response) {
     const albumHash = request.params.albumHash;
 
     axios({
@@ -34,15 +34,15 @@ exports.getAlbumInfo = function (request: any, response: any) {
             'Authorization': 'Client-ID ' + clientId
         },
     })
-        .then(function (resp: any) {
+        .then(function (resp: AxiosResponse) {
             response.send(resp.data);
         })
-        .catch(function (err: any) {
+        .catch(function (err: AxiosError) {
             response.send(err);
         });
 };
 
-exports.getAlbumImages = function (request: any, response: any) {
+exports.getAlbumImages = function (request: Request, response: Response) {
     const albumHash = request.params.albumHash;
 
     axios({
@@ -52,24 +52,24 @@ exports.getAlbumImages = function (request: any, response: any) {
              'Authorization': 'Client-ID ' + clientId
         }
     })
-        .then(function (resp: any) {
+        .then(function (resp: AxiosResponse) {
             response.send(resp.data);
         })
-        .catch(function (err: any) {
+        .catch(function (err: AxiosError) {
             response.send(err);
         });
 };
 
-exports.getAlbums = function (request: any, response: any) {
+exports.getAlbums = function (request: Request, response: Response) {
 
     axios.get(
         'https://api.imgur.com/3/account/triponlife/albums',
         {'headers': {'Authorization': 'Bearer ' + accessToken}}
     )
-        .then(function (resp: any) {
+        .then(function (resp: AxiosResponse) {
             response.send(resp.data);
         })
-        .catch(function (err: any) {
+        .catch(function (err: AxiosError) {
             response.send(err);
         });
 };
