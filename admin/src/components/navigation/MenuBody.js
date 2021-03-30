899fb465577b4000cc29059c835db3c824be853b
cassiopia <cassiopia@yandex.ru>
2021-03-30 15:05:47 +0300

    -заменила логитип на клиенте и админке; сократила количество пунктов в меню админа;

--- admin/src/components/navigation/MenuBody.js
+++ admin/src/components/navigation/MenuBody.js
@@ -5,18 +5,11 @@ import MenuPortfolio from './MenuPortfolio'
 export default function MenuBody() {
     return (
         <ul className="nav navbar-nav navbar-right menu-effect">
-            <li className="current"><a href="/" data-hover="Домашняя">Домашняя</a></li>
             <li>
                 <Link to="/portfolio" data-hover="Портфолио">Портфолио</Link>
             </li>
             <li>
-                <Link to="/page/portfolio" data-hover="Новый тип страницы">Новый тип страницы</Link>
-            </li>
-            <li>
                 <Link to="/blog" data-hover="Блог">Блог</Link>
-            </li>
-            <li>
-                <Link to="/contact" data-hover="Контакты">Контакты</Link>
             </li>
         </ul>
     );
--- admin/src/components/routes/Route.js
+++ admin/src/components/routes/Route.js
@@ -3,14 +3,10 @@ import {Switch, Route as ReactRouter, Redirect} from 'react-router-dom'
 import AuthenticationLayoutRoute from '../pages/_layouts/AuthenticationLayout';
 import DefaultLayoutRoute from '../pages/_layouts/DefaultLayout';
 
-const Home = lazy(() => import("../pages/home/Home"));
-const About = lazy(() => import("../pages/about/About"));
 const Blog = lazy(() => import("../pages/blog/Blog"));
 const ArticleDetails = lazy(() => import("../pages/blog/ArticleDetails"));
-const Contact = lazy(() => import("../pages/contact/Contact"));
 const Portfolio = lazy(() => import("../pages/portfolio/Portfolio"));
 const AlbumDetails = lazy(() => import("../pages/portfolio/AlbumDetails"));
-// const LoginPage = lazy(() => import("../pages/authentication/LoginPage"));
 const Login = lazy(() => import("../pages/authentication/Login"));
 const Registration = lazy(() => import("../pages/authentication/Registration"));
 
@@ -20,16 +16,13 @@ class Route extends Component {
         return (
             <Switch>
                 <Suspense style="color:red" fallback={<div>Loading...</div>}>
-                    <ReactRouter exact path="/" component={Home}/>
                     <ReactRouter exact path="/">
                         <Redirect to="/login"/>
                     </ReactRouter>
                     <AuthenticationLayoutRoute path="/login" component={Login}/>
                     <AuthenticationLayoutRoute path="/registration" component={Registration}/>
-                    <DefaultLayoutRoute path="/about" component={About}/>
                     <DefaultLayoutRoute path="/blog" component={Blog}/>
                     <DefaultLayoutRoute path="/article-details" component={ArticleDetails}/>
-                    <DefaultLayoutRoute path="/contact" component={Contact}/>
                     <DefaultLayoutRoute path="/portfolio" component={Portfolio}/>
                     <DefaultLayoutRoute path="/album-details" component={AlbumDetails}/>
                 </Suspense>
