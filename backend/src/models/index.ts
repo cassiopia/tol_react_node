0e16b1cd34ce0138a844c9303483521a54bbedc8
cassiopia <cassiopia@yandex.ru>
2021-11-28 17:27:30 +0300

    - Исправила файл создания БД

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -7,57 +7,49 @@ import {Role} from "./Role.model";
 import {User} from "./User.model";
 import {UserRole} from "./UserRole.model";
 
-Page.belongsToMany(Tag, {
-    through: PageTag,
-    foreignKey: 'page_id'
-});
+(async () => {
 
-Tag.belongsToMany(Page, {
-    through: PageTag,
-    foreignKey: 'tag_id'
-});
+    Page.belongsToMany(Tag, {
+        through: PageTag,
+        foreignKey: 'page_id'
+    });
 
-Page.hasMany(PageImage, {
-    foreignKey: 'page_id'
-});
+    Tag.belongsToMany(Page, {
+        through: PageTag,
+        foreignKey: 'tag_id'
+    });
 
-PageImage.belongsTo(Page, {
-    foreignKey: 'page_id'
-});
+    Page.hasMany(PageImage, {
+        foreignKey: 'page_id'
+    });
+
+    PageImage.belongsTo(Page, {
+        foreignKey: 'page_id'
+    });
 
 // todo Если будут глюки -- смотреть тут ;)
-Page.hasMany(PageTag, {
-    foreignKey: 'page_id'
-});
+    Page.hasMany(PageTag, {
+        foreignKey: 'page_id'
+    });
 
-PageTag.belongsTo(Page, {
-    foreignKey: 'page_id'
-});
+    PageTag.belongsTo(Page, {
+        foreignKey: 'page_id'
+    });
 
-Role.belongsToMany(User, {
-    through: UserRole,
-    foreignKey: "role_id"
-});
+    Role.belongsToMany(User, {
+        through: UserRole,
+        foreignKey: "role_id"
+    });
 
-User.belongsToMany(Role, {
-    through: UserRole,
-    foreignKey: "user_id"
-});
+    User.belongsToMany(Role, {
+        through: UserRole,
+        foreignKey: "user_id"
+    });
 
-Role.create({
-    id: 1,
-    name: "user"
-});
 
-Role.create({
-    id: 2,
-    name: "moderator"
-});
-
-Role.create({
-    id: 3,
-    name: "admin"
-});
-
-//database.sync({ force: true }).then(() => console.log("All models were synchronized successfully."));
-database.sync().then(() => console.log("All models were synchronized successfully."));
+//await database.sync({ force: true });
+//console.log("All models were synchronized successfully.");
+    database.sync().then(() => console.log("All models were synchronized successfully."));
+    const defaultRoles = [{id: 1, name: "user"}, {id: 2, name: "moderator"}, {id: 3, name: "admin"}];
+    await Role.bulkCreate(defaultRoles, {ignoreDuplicates: true});
+})();
